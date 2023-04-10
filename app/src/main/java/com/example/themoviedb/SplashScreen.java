8cbdbebdfae4d76ca060b6ed9147116dc66d6662
Diego Carrascal <diegocarrascal@wposs.com>
2023-04-10 14:47:30 -0500

    IMPLEMENTACIÓN - SplashScreen
    * Se agrega SplashScreen
    * Se agrega logo de The Movie DB

--- app/src/main/java/com/example/themoviedb/SplashScreen.java
+++ app/src/main/java/com/example/themoviedb/SplashScreen.java
@@ -2,7 +2,11 @@ package com.example.themoviedb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.themoviedb.View.Movies;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+
+        new Handler().postDelayed(() -> {
+            Intent intent= new Intent(this, Movies.class);
+            startActivity(intent);
+            finish();
+        },3000);
+
+
     }
 }
